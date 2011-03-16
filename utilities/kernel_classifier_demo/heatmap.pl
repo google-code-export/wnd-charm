5b6ddd08719ea2acd97e5ba2892481afe0a2d5a1
colettace <colettace@d860a611-dd6a-fbde-3e4e-7fa7191099b5>
2011-03-16 17:12:00 +0000

    heatmap: partial checkin. Added Ilya's imagewriter code, and functions to save and load files with marg probs, so you don't have to rerun wndchrm all the time.

--- utilities/kernel_classifier_demo/heatmap.pl
+++ utilities/kernel_classifier_demo/heatmap.pl
@@ -3,9 +3,11 @@
 use strict;
 use warnings;
 use Getopt::Std;
+use Image::Magick;
 
-#wndchrm_debug classify  -l -B1112,760,280,280 NewNotRotated-ml.fit G\ -\ 9\(fld\ 37\ wv\ TL\ -\ DIC\ -\ Open\).tif
-
+#################################################################################
+#
+#################################################################################
 sub RunWNDCHRM_atROI($$$$)
 {
 	my $X = $_[0];
@@ -34,43 +36,152 @@ sub RunWNDCHRM_atROI($$$$)
 	return -1;
 }
 
-sub PrintLetter {
-	my $aref = shift;
-	my $max = 0;
-	foreach (@$aref) {
-		if( $_ > $max ) {
-			$max = $_;
+#################################################################################
+#
+#################################################################################
+sub writeImage( \@ ) {
+
+	my @marg_probs = $_[0];
+	my $num_cols = $_[1];
+	my $num_rows = $_[2];
+	my $image = Image::Magick->new;
+	my $res;
+	
+# This seems to be completely ignored.  The type is defined from the pixel values (!)
+	$res = $image->Set(type=>'TrueColorMatte');
+	warn "$res" if $res;
+	$res = $image->Set(size=>"$width".'x'."$height");
+	warn "$res" if $res;
+	$res = $image->Set(depth=>8);
+	warn "$res" if $res;
+# These are conveniently ignored...
+# use tiffcp -f msb2lsb for fill order
+	$res = $image->Set(option=>'tiff:alpha=associated');
+	warn "$res" if $res;
+	$res = $image->Set(option=>'tiff:fill-order:msb2lsb');
+	warn "$res" if $res;
+	my @pixels;
+	my ($row,$col);
+# This mysterious call supersedes anything we do with setting the image type.
+# It has to be transparent or the alpha channel is ignored
+# Also, if we fail to do something like this, the image will be undefined after writing pixels to it.
+# Of course, no error is reported while writing pixels - only when writing the file.
+	$res = $image->Read ('CANVAS:transparent');
+	warn "$res" if $res;
+
+	my $r = 0, $g = 0, $b = 0, $a = 0;
+	for( $row = 0; $row < $num_rows; $row++ ) {
+		for( $col = 0; $col < $num_cols; $col++ ) {
+		# For maximum efficiency, we can't set the RGB and Alpha in one call.
+			$a = $marg_probs[$col][$row]->[0];
+			@pixels = ( $a );
+			$res = $image->SetPixel(channel => 'Alpha', 'x' => $x, 'y' => $y, 'color' => \@pixels);
+			warn "$res" if $res;
+
+			$r = $marg_probs[$col][$row]->[1];
+			$g = $marg_probs[$col][$row]->[2];
+			$b = $marg_probs[$col][$row]->[3];
+			@pixels = ( $r, $g, $b );
+			$res = $image->SetPixel(channel => 'RGB', 'x' => $x, 'y' => $y, 'color' => \@pixels);
+			warn "$res" if $res;
 		}
 	}
+	printf "\n";
+	$res = $image->Write(filename=>'image.png');
+	$res = $image->Write(filename=>'image.tiff');
+	warn "$res" if $res;
+}
 
-	if( $$aref[0] == $max ) {
-		print 'B';
-	} elsif( $$aref[1] == $max ) {
-		print 'D';
-	} elsif( $$aref[2] == $max ) {
-		print 'H';
-	} else {
-		print 'T';
+#################################################################################
+#
+#################################################################################
+sub OutputASCIIpic( \@ ) {
+	my @marg_probs = $_[0];
+	my $num_cols = $#marg_probs;
+	my $num_rows = $#{ $marg_probs[0] };
+
+	for( my $row = 0; $row <= $num_rows; $row++ ) {
+		for( my $col = 0; $col <= $num_cols; $col++ ) {
+			my $max = 0;
+			my $index = 0
+			my $index_of_highest = -1;
+			foreach( @{ $marg_probs[$col][$row] } ) {
+				if( $_ > $max ) {
+					$max = $_;
+					$index_of_highest = $index;
+				}
+				$index++;
+			}
+			if( $index_of_highest == 0 ) {
+				print 'B';
+			} elsif( $index_of_highest == 1 ) {
+				print 'D';
+			} elsif( $index_of_highest == 2 ) {
+				print 'H';
+			} elsif( $index_of_highest == 3 ) {
+				print 'T';
+			} else {
+				print '?';
+			}
+		}
+		print "\n";
 	}
 }
 
-sub main {
+#################################################################################
+# LoadInputFile( @matrix, $input_file )
+#################################################################################
+sub LoadInputFile( \@$ ) {
+	my @results_matrix = $_[0];
+	my $input_file = $_[1];
 
+	open( IN, '<', $input_file ) or die "Couldn't open input file $input_file: $!\n";
+
+	while( <IN> ) {
+		if( /col (\d+), row (\d+): \((\d\.\d+)\s+(\d\.\d+)\s+(\d\.\d+)\s+(\d\.\d+)\)/ ) {
+			print "loading col $1, row $2, marg probs( $3, $4, $5, $6 )\n";
+			@{ $results_matrix[$1][$2] } = ( $3, $4, $5, $6 );
+		}
+	}
+	close IN;
+}
+
+#################################################################################
+# WriteOutputFile( @matrix, $output_file )
+#################################################################################
+sub WriteOutputFile( \@$ ) {
+	my @results_matrix = $_[0];
+	my $output_file = $_[1];
+
+	my $num_cols = $#results_matrix;
+	my $num_rows = $#{ $results_matrix[0] };
+
+	open( OUT, '>', $output_file ) or die "Couldn't open output file $output_file: $!\n";
+
+	for( my $col = 0; $col <= $num_cols; $col++ ) {
+		for( my $row = 0; $row <= $num_rows; $row++ ) {
+			print OUT "col $col, row $row: (";
+			foreach( @{ $results_matrix[$col][$row] } ) {
+				print OUT "$_\t";
+			}
+			print OUT ")\n";
+		}
+	}
+	close OUT;
+}
+
+#################################################################################
+# RunKernelScan( @matrix, $starting_fraction )
+#################################################################################
+sub RunKernelScan( \@$ ) {
+
+	my @results_matrix = $_[0];
+	my $starting_point = $_[1];
 	my $image_width = 1392;
 	my $image_height = 1040;
 	my $kernel_width = 280;
 	my $kernel_height = 280;
 	my $granularity = 30;
-
-	my $starting_point = 0; # a number from 0 to 1 signifying percentage
-	                    # where this instance should start calculating sigs.
-	my %opts;
-	if( getopts( 's:', \%opts ) ) {
-		print "found starting point: $opts{'s'}\n";
-		$starting_point = $opts{'s'};
-	} else {
-		$starting_point = 0;
-	}
 
 	my $deltaX = int( $image_width / $granularity );
 	my $deltaY = int( $image_height / $granularity );
@@ -85,7 +196,6 @@ sub main {
 
 	my $x = 0;
 	my $y = 0;
-	my @results_matrix;
 
 	for( my $col = $starting_col; $col <= $num_cols; $col++ ) {
 		for( my $row = 0; $row <= $num_rows; $row++ ) {
@@ -95,13 +205,48 @@ sub main {
 			@{ $results_matrix[$col][$row] } = RunWNDCHRM_atROI( $x, $y, $kernel_width, $kernel_height );
 		}
 	}
+}
 
-	for( my $x = $starting_col; $x <= $#results_matrix; $x++ ) {
-		for( my $y = 0; $y <= $#{ $results_matrix[0] }; $y++ ) {
-			PrintLetter( \@{ $results_matrix[$x][$y] } );
+#################################################################################
+# main()
+#################################################################################
+sub main {
+
+	my $starting_point = 0; # a number from 0 to 1 signifying percentage
+	                        # where this instance should start calculating sigs.
+	my $input_file = undef;
+	my $output_file = undef;
+	my %opts;
+	my @results_matrix;
+
+	if( getopts( 's:o:i:', \%opts ) ) {
+		if( defined $opts{'s'} ) {
+			print "found starting point: $opts{'s'}\n";
+			$starting_point = $opts{'s'};
 		}
-		print "\n";
+		if( defined $opts{'o'} ) {
+			print "marginal probs will be output to file $opts{'s'}\n";
+			$output_file = $opts{'o'};
+		}
+		if( defined $opts{'i'} ) {
+			print "marginal probs inputted from file $opts{'i'}\n";
+			$input_file = $opts{'i'};
+		}
 	}
+
+	if( defined $input_file ) {
+		LoadInputFile( @results_matrix, $input_file );
+	} else {
+		RunKernelScan( @results_matrix, $starting_point );
+	}
+
+	if( defined $output_file ) {
+		WriteOutputFile( @results_matrix, $output_file );
+	}
+
+	OutputASCIIpic( @results_matrix );
+
+	writeImage( @results_matrix );
 	return 0;
 }
 
