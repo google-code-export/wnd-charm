e88fd0f9b4924e0aed58d04369d08464269aeca0
colettace <colettace@d860a611-dd6a-fbde-3e4e-7fa7191099b5>
2011-03-17 21:40:19 +0000

    heatmap.pl: took a lot of hardcoded options out and made them as command line arguments.

--- utilities/kernel_classifier_demo/heatmap.pl
+++ utilities/kernel_classifier_demo/heatmap.pl
@@ -6,51 +6,20 @@ use Getopt::Std;
 use Image::Magick;
 
 #################################################################################
-#
+# #FIXME: The pixel values are hard coded for a 4 class problem
 #################################################################################
-sub RunWNDCHRM_atROI($$$$)
-{
-	my $X = $_[0];
-	my $Y = $_[1];
-	my $deltaX = $_[2];
-	my $deltaY = $_[3];
+sub writeImage {
 
-	my $wndchrm = "~/src/iicbu/wndchrm/branches/wndchrm-1.30/wndchrm";
-	my $training_fit = "NewNotRotated-ml.fit";
-	my $test_image = 'G\ -\ 9\(fld\ 37\ wv\ TL\ -\ DIC\ -\ Open\).tif';
-
-	my $test_image_reg_exp = 'G - 9\(fld 37 wv TL - DIC - Open\)\.tif';
-
-	my $cmd = "$wndchrm classify -l -s1 -B$X,$Y,$deltaX,$deltaY $training_fit $test_image";
-	print "Running wndchrm command:\n $cmd \n";
-	my @output = `$cmd`;
-
-	#print "Here was the output: $output\n\n";
-	foreach (@output) {
-		if( /^$test_image_reg_exp\s+\S+\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)/ ) {
-			print "Caught marginal probabilities $1, $2, $3, $4\n";
-			return ($1, $2, $3, $4 );
-		}
-	}
-
-	return -1;
-}
-
-#################################################################################
-#
-#################################################################################
-sub writeImage( \@ ) {
-
-	my @marg_probs = $_[0];
-	my $num_cols = $_[1];
-	my $num_rows = $_[2];
+	my $marg_probs_ref = shift;
+	my $num_cols = $#{ $marg_probs_ref };
+	my $num_rows = $#{ $marg_probs_ref->[0] };
 	my $image = Image::Magick->new;
 	my $res;
 	
 # This seems to be completely ignored.  The type is defined from the pixel values (!)
 	$res = $image->Set(type=>'TrueColorMatte');
 	warn "$res" if $res;
-	$res = $image->Set(size=>"$width".'x'."$height");
+	$res = $image->Set(size=>"$num_cols".'x'."$num_rows");
 	warn "$res" if $res;
 	$res = $image->Set(depth=>8);
 	warn "$res" if $res;
@@ -69,20 +38,23 @@ sub writeImage( \@ ) {
 	$res = $image->Read ('CANVAS:transparent');
 	warn "$res" if $res;
 
-	my $r = 0, $g = 0, $b = 0, $a = 0;
+	my $r = 0;
+	my $g = 0;
+	my $b = 0;
+	my $a = 0;
 	for( $row = 0; $row < $num_rows; $row++ ) {
 		for( $col = 0; $col < $num_cols; $col++ ) {
 		# For maximum efficiency, we can't set the RGB and Alpha in one call.
-			$a = $marg_probs[$col][$row]->[0];
+			$a = $$marg_probs_ref[$col][$row]->[0];
 			@pixels = ( $a );
-			$res = $image->SetPixel(channel => 'Alpha', 'x' => $x, 'y' => $y, 'color' => \@pixels);
+			$res = $image->SetPixel(channel => 'Alpha', 'x' => $col, 'y' => $row, 'color' => \@pixels);
 			warn "$res" if $res;
 
-			$r = $marg_probs[$col][$row]->[1];
-			$g = $marg_probs[$col][$row]->[2];
-			$b = $marg_probs[$col][$row]->[3];
+			$r = $$marg_probs_ref[$col][$row]->[1];
+			$g = $$marg_probs_ref[$col][$row]->[2];
+			$b = $$marg_probs_ref[$col][$row]->[3];
 			@pixels = ( $r, $g, $b );
-			$res = $image->SetPixel(channel => 'RGB', 'x' => $x, 'y' => $y, 'color' => \@pixels);
+			$res = $image->SetPixel(channel => 'RGB', 'x' => $col, 'y' => $row, 'color' => \@pixels);
 			warn "$res" if $res;
 		}
 	}
@@ -93,19 +65,20 @@ sub writeImage( \@ ) {
 }
 
 #################################################################################
-#
+# #FIXME: The ASCII output is hardcoded for a 4 class problem,
+#         with a specific order of specific classes in the marginal probabilities
 #################################################################################
-sub OutputASCIIpic( \@ ) {
-	my @marg_probs = $_[0];
-	my $num_cols = $#marg_probs;
-	my $num_rows = $#{ $marg_probs[0] };
+sub OutputASCIIpic {
+	my $marg_probs_ref = shift;
+	my $num_cols = $#{ $marg_probs_ref };
+	my $num_rows = $#{ $marg_probs_ref->[0] };
 
 	for( my $row = 0; $row <= $num_rows; $row++ ) {
 		for( my $col = 0; $col <= $num_cols; $col++ ) {
 			my $max = 0;
-			my $index = 0
+			my $index = 0;
 			my $index_of_highest = -1;
-			foreach( @{ $marg_probs[$col][$row] } ) {
+			foreach( @{ $$marg_probs_ref[$col][$row] } ) {
 				if( $_ > $max ) {
 					$max = $_;
 					$index_of_highest = $index;
@@ -129,39 +102,46 @@ sub OutputASCIIpic( \@ ) {
 }
 
 #################################################################################
-# LoadInputFile( @matrix, $input_file )
+# LoadInputFile()
+#       inputs: $input_file
+#       output: @results_matrix
+# #FIXME: This is also a 4-class application specific function
 #################################################################################
-sub LoadInputFile( \@$ ) {
-	my @results_matrix = $_[0];
-	my $input_file = $_[1];
+sub LoadInputFile {
+	my $input_file = shift;
+	my @results_matrix;
 
 	open( IN, '<', $input_file ) or die "Couldn't open input file $input_file: $!\n";
 
+	# ex: "col 7, row 4: (0.000	0.810	0.188	0.003	)"
+
 	while( <IN> ) {
-		if( /col (\d+), row (\d+): \((\d\.\d+)\s+(\d\.\d+)\s+(\d\.\d+)\s+(\d\.\d+)\)/ ) {
+		if( /col (\d+), row (\d+): \((\d\.\d+)\s+(\d\.\d+)\s+(\d\.\d+)\s+(\d\.\d+)/ ) {
 			print "loading col $1, row $2, marg probs( $3, $4, $5, $6 )\n";
 			@{ $results_matrix[$1][$2] } = ( $3, $4, $5, $6 );
 		}
 	}
 	close IN;
+	return @results_matrix;
 }
 
 #################################################################################
-# WriteOutputFile( @matrix, $output_file )
+# WriteOutputFile( \@matrix, $output_file )
 #################################################################################
-sub WriteOutputFile( \@$ ) {
-	my @results_matrix = $_[0];
-	my $output_file = $_[1];
+sub WriteOutputFile{
+	my ($results_matrix_ref, $output_file) = @_;
 
-	my $num_cols = $#results_matrix;
-	my $num_rows = $#{ $results_matrix[0] };
+	my $num_cols = $#{ $results_matrix_ref };
+	my $num_rows = $#{ $results_matrix_ref->[0] };
+
+	print "Writing output file: $output_file with $num_cols columns and $num_rows rows.\n";
 
 	open( OUT, '>', $output_file ) or die "Couldn't open output file $output_file: $!\n";
 
 	for( my $col = 0; $col <= $num_cols; $col++ ) {
 		for( my $row = 0; $row <= $num_rows; $row++ ) {
 			print OUT "col $col, row $row: (";
-			foreach( @{ $results_matrix[$col][$row] } ) {
+			foreach( @{ $$results_matrix_ref[$col][$row] } ) {
 				print OUT "$_\t";
 			}
 			print OUT ")\n";
@@ -171,29 +151,37 @@ sub WriteOutputFile( \@$ ) {
 }
 
 #################################################################################
-# RunKernelScan( @matrix, $starting_fraction )
+# RunKernelScan()
+#   required inputs: $path_to_wndchrm, $image_name, $training_set_fit_file, $deltaX, $deltaY
+#   optional inputs: $starting_fraction
+#   output:          @results_matrix
 #################################################################################
-sub RunKernelScan( \@$ ) {
+sub RunKernelScan( $$$$$;$ ) {
 
-	my @results_matrix = $_[0];
-	my $starting_point = $_[1];
-	my $image_width = 1392;
+	my( $path_to_wndchrm, $test_image, $training_fit, $deltaX, $deltaY, $starting_point )= @_;
+	my $image_width = 1392; #FIXME: use tiffinfo to automatically extract image pixel dimensions
 	my $image_height = 1040;
-	my $kernel_width = 280;
+	my $kernel_width = 280; #FIXME: this should be a command line input
 	my $kernel_height = 280;
-	my $granularity = 30;
 
-	my $deltaX = int( $image_width / $granularity );
-	my $deltaY = int( $image_height / $granularity );
+	my @results_matrix;
 
-	my $col = 0;
-	my $row = 0;
+  my $test_image_shell = $test_image;
+	my $test_image_reg_exp = $test_image;
+
+	$test_image_shell =~ s/([ \(\)])/\\$1/g; # for shell, spaces and parentheses need to be escaped
+	$test_image_reg_exp =~ s/([\(\)\.])/\\$1/g; # for regular expressions, parens and periods need to be escaped
+
+	my $retval = 0;
 
 	my $num_cols = int( ($image_width - $kernel_width) / $deltaX );
 	my $num_rows = int( ($image_height - $kernel_height) / $deltaY );
 
 	my $starting_col = int( $starting_point * $num_cols );
+	my $remainder = ( $starting_point * $num_cols ) - $starting_col;
+	my $starting_row = int( $remainder * $num_rows );
 
+	print "Image will be scanned with $num_cols columns and $num_rows rows, starting at column $starting_col\n";
 	my $x = 0;
 	my $y = 0;
 
@@ -202,9 +190,55 @@ sub RunKernelScan( \@$ ) {
 			$x = $col * $deltaX;
 			$y = $row * $deltaY;
 			print "col $col, row $row, x: $x, y: $y, kernel width: $kernel_width, kernel height: $kernel_height\n";
-			@{ $results_matrix[$col][$row] } = RunWNDCHRM_atROI( $x, $y, $kernel_width, $kernel_height );
+
+			my $cmd = "$path_to_wndchrm classify -l -s1 -B$x,$y,$deltaX,$deltaY $training_fit $test_image_shell 2>&1";
+			print "Running wndchrm command:\n $cmd \n";
+			my @output = `$cmd`;
+			$retval = $? >> 8;
+
+			if( $retval != 1 ) {
+				print "WNDCHRM returned error: $retval\n";
+				print "Wndchrm output:\n";
+				foreach (@output) { print "$_\n"; }
+				print "\n";
+				die;
+			}
+			#print "Here was the output: $output\n\n";
+			foreach (@output) {
+				if( /^$test_image_reg_exp\s+\S+\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)/ ) {
+					print "Caught marginal probabilities $1, $2, $3, $4\n";
+					@{ $results_matrix[$col][$row] } = ($1, $2, $3, $4 );
+				}
+			}
 		}
 	}
+	return @results_matrix;
+}
+
+#################################################################################
+#
+#################################################################################
+sub ShowHelp {
+	print "WNDCHRM heatmap generator, ver 1.30\n\n";
+	print "usage: heatmap.pl -i input_image.tiff -w /path/to/wndchrm -t /path/to/training_set.fit -p 20x20\n";
+	print "\n";
+	print "Required arguments:\n";
+	print "-i <path>  : Input image: the image which will be scanned.\n";
+	print "-w <path>  : WNDCHRM path: path to the wndchrm executable.\n";
+	print "-t <path>  : Training Set: path to the .fit file containing the classifier.\n";
+	print "             must be generated beforehand usind WNDCHRM\n";
+	print "-p <#x#>   : Pixel dimensions: Defining the resolution of the heatmap image\n";
+	print "             one heatmap pixel will represent NxN original image pixels\n";
+	print "\n";
+	print "additional optional arguments:\n";
+	print "-s <#>     : Starting point: where # is a decimal between 0.0 and 1.0 indicating where this instance of\n";
+	print "             the script should start calculating features for given input image\n";
+	print "-d <path>  : Dump marginal probabilities to this file to save time when re-running\n";
+	print "             this script later.\n";
+	print "-l <path>  : Load a file containing dumped marginal probabilities created by previously running this script\n";
+	print "             using the -d option.\n";
+	print "-o <path>  : Specify a filename and path to the generate heatmap image.\n";
+	print "           : otherwise, image is created with deafult name \"image.png\"\n";
 }
 
 #################################################################################
@@ -216,38 +250,77 @@ sub main {
 	                        # where this instance should start calculating sigs.
 	my $input_file = undef;
 	my $output_file = undef;
+	my $path_to_wndchrm = undef;
+	my $input_image = undef;
+	my $training_fit = undef;
+	my $heatmap_image_path = undef;
+	my $heatmap_resolution_string = undef;
 	my %opts;
 	my @results_matrix;
 
-	if( getopts( 's:o:i:', \%opts ) ) {
-		if( defined $opts{'s'} ) {
-			print "found starting point: $opts{'s'}\n";
-			$starting_point = $opts{'s'};
+	if( getopts( 'w:i:t:p:;s:d:l:o:', \%opts ) ) {
+		$path_to_wndchrm = $opts{'w'};
+		$input_image = $opts{'i'};
+		$training_fit = $opts{'t'};
+		$heatmap_resolution_string = $opts{'p'};
+		$starting_point = $opts{'s'};
+		$output_file = $opts{'d'};
+		$input_file = $opts{'l'};
+		$heatmap_image_path = $opts{'o'};
+	} else {
+		&ShowHelp;
+		return -1;
+	}
+
+	if( !$path_to_wndchrm || !$input_image || !$training_fit || !$heatmap_resolution_string ) {
+		&ShowHelp;
+		return -1;
+	}
+	else {
+		print "path to wndchrm: $path_to_wndchrm\n";
+		print "input image: $input_image\n";
+		print "training set: $training_fit\n";
+		print "heatmap resolution: $heatmap_resolution_string\n";
+		if( $starting_point ) {
+			print "found starting point: $starting_point\n";
 		}
-		if( defined $opts{'o'} ) {
-			print "marginal probs will be output to file $opts{'s'}\n";
-			$output_file = $opts{'o'};
+		if( $output_file ) {
+			print "marginal probs will be output to file $output_file\n";
 		}
-		if( defined $opts{'i'} ) {
-			print "marginal probs inputted from file $opts{'i'}\n";
-			$input_file = $opts{'i'};
+		if( $input_file ) {
+			print "marginal probs inputted from file $input_file\n";
 		}
+		if( $heatmap_image_path ) {
+			print "Heatmap image will be named: $heatmap_image_path\n";
+		}
+	}
+
+	my( $deltaX, $deltaY );
+	if( $heatmap_resolution_string =~ /(\d+)x(\d+)/ ) {
+		$deltaX = $1;
+		$deltaY = $2;
+	} else {
+		print "Incorrectly formatted pixel dimensions ($heatmap_resolution_string)\n";
+		print "Please enxer pixel dimension in the form of #x#, for example, 20x20\n";
+		return -2;
 	}
 
 	if( defined $input_file ) {
-		LoadInputFile( @results_matrix, $input_file );
+		@results_matrix = LoadInputFile( $input_file );
 	} else {
-		RunKernelScan( @results_matrix, $starting_point );
+		@results_matrix = RunKernelScan( $path_to_wndchrm, $input_image, $training_fit, $deltaX, $deltaY, $starting_point );
 	}
+
+	print "Loaded results matrix with $#results_matrix columns and $#{ $results_matrix[0] } rows.\n";
 
 	if( defined $output_file ) {
-		WriteOutputFile( @results_matrix, $output_file );
+		WriteOutputFile( \@results_matrix, $output_file );
 	}
 
-	OutputASCIIpic( @results_matrix );
+	OutputASCIIpic( \@results_matrix );
 
-	writeImage( @results_matrix );
-	return 0;
+	writeImage( \@results_matrix, $heatmap_image_path );
+	return 1;
 }
 
 &main;
