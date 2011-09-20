4eed5efb7e750c5b2ef9794ee6caf26f74493e74
christopher.coletta@nih.gov <christopher.coletta@nih.gov@d860a611-dd6a-fbde-3e4e-7fa7191099b5>
2011-09-20 20:55:04 +0000

    Leave one out now takes three arguments: 1. path to a standard wndcharm file of files 2. full path to wndcharm executable 3. the desired wndcharm parameters, enclosed in quotes Also, this script is now compatible with the newer version of wndcharm html reports that give class accuracies and error bars and computes error bars for results returned here as well.

--- utilities/leave_one_out/leave_one_out.pl
+++ utilities/leave_one_out/leave_one_out.pl
@@ -7,10 +7,25 @@ sub uniq {
     return keys %{{ map { $_ => 1 } @_ }};
 }
 
-my $orig = shift;
-print $orig . "\n";
+my $usage = "\nUSAGE (three mandatory arguments):\nleave_one_out.pl <file of files> <full path to wndcharm> \"<[test|classify] wndcharm parameters>\"\n";
+my $orig = shift; #monkey_brain_CA1.txt
+if( !defined $orig or $orig eq "" ){
+	die "error with file of files\n$usage";
+}
+print "Reading in file $orig\n";
 
-my $wndcharm_path = "~/src/wnd-charm.googlecode.com/wnd-charm/wndchrm/tags/wndchrm-1.31/wndchrm";
+my $wndcharm_path = shift; #"/Users/chris/src/wnd-charm.googlecode.com/wnd-charm/wndchrm/trunk/wndchrm";
+if( !defined $wndcharm_path or $wndcharm_path eq "" ) {
+	die "error with wndcharm path\n$usage";
+}
+print "Using path to wndcharm: $wndcharm_path\n";
+
+my $wndcharm_params = shift; # "test -l -r1 -i10 -n101 -S2201";
+if( !defined $wndcharm_params or $wndcharm_params eq "" ) {
+	die "error with wndcharm parameters\n$usage";
+}
+print "Using wndcharm parameters: $wndcharm_params\n";
+
 my $cmd;
 
 die "must provide master wndcharm input file of files\n" if (!defined $orig);
@@ -24,7 +39,7 @@ my %groups;
 my %class_list;
 my $basename;
 
-open IN, $orig or die;
+open IN, $orig or die "Couldn't open $orig... dying.\n";
 foreach my $line (<IN>) {
 	if ( $line =~ /(.*)_CR.*\t(\S+)$/ ) {
 		push @{ $groups{$1} }, $line;
@@ -51,13 +66,16 @@ my %num_individuals;
 
 my $class;
 
+
 foreach $class ( sort &uniq( values %class_list ) ) {
+	print $class . "\n";
 	$class_count = 0;
 	@class_marg_probs = ();
 	@class_confusion_matrix = ();
 	print OUT "<hr><p><h2>Individual results from class $class</h2></p>\n";
 	foreach my $group_name (sort keys %groups) {
 		next if( $class_list{$group_name} ne $class );
+		print "\t" . $group_name . "\n";
 
 		++$class_count;
 		$table = "";
@@ -70,54 +88,102 @@ foreach $class ( sort &uniq( values %class_list ) ) {
 		my $test_file = "$container_dir/$group_name/${group_name}_only.txt";
 		my $test_html = "$container_dir/$group_name/${group_name}_only.html";
 
-		open LEFT_OUT, ">", $test_file or die;
-		open ALL_ELSE, ">", $training_file or die;
-		foreach my $group (keys %groups) {
-			if( $group eq $group_name ) {
-				foreach my $img_name ( @{ $groups{ $group } } ) {
-					print LEFT_OUT $img_name;
+		$cmd = "$wndcharm_path $wndcharm_params $training_file $test_file $test_html";
+		my $report_beginning_section;
+
+		print "\t\tlooking for $test_html...";
+		if( -e $test_html ) {
+			# Open the html and see if it was created with the same parameters specified here
+			print " found. ";
+			open IN, $test_html or die;
+
+			$report_beginning_section = "";
+			while (<IN>) {
+				if( /NAME=\"split0\"/ ) {
+					last;
 				}
+				$report_beginning_section .= $_;
 			}
-			else {
-				foreach my $img_name ( @{ $groups{ $group } } ) {
-					print ALL_ELSE $img_name;
+			# grab everything inside the command line <pre>....</pre>
+			# skip leading whitespace
+			if( $report_beginning_section =~ /<pre>\s*(.*?)<\/pre>/s ) {
+				my $already_ran_command = $1;
+				if( $already_ran_command ne $cmd ) {
+					my $error_msg =  "***FATAL ERROR:\n";
+					$error_msg .= "wndcharm has already been run for this data set but with different parameters:\n";
+					$error_msg .= "You wanted this: \n";
+					$error_msg .= "\t$cmd\n";
+					$error_msg .= "You have this: \n";
+					$error_msg .= "\t$already_ran_command\n";
+					$error_msg .= "Please move the old dataset out of the way\n dying...\n";
+					die	$error_msg;
+				}
+				else
+				{
+					print " Parameters match. \n"
 				}
 			}
 		}
-		close LEFT_OUT;
-		close ALL_ELSE;
+		else
+		{
+			# have to run wndcharm from scratch
+			print " not found.\n";
 
-		$cmd = "$wndcharm_path test -l -r1 -i10 -n100 $training_file $test_file $test_html";
-		print "\n\n\n========================================================================\n";
-		print $cmd . "\n\n";
-		system( $cmd );
+			open LEFT_OUT, ">", $test_file or die;
+			open ALL_ELSE, ">", $training_file or die;
+			foreach my $group (keys %groups) {
+				if( $group eq $group_name ) {
+					foreach my $img_name ( @{ $groups{ $group } } ) {
+						print LEFT_OUT $img_name;
+					}
+				}
+				else {
+					foreach my $img_name ( @{ $groups{ $group } } ) {
+						print ALL_ELSE $img_name;
+					}
+				}
+			}
+			close LEFT_OUT;
+			close ALL_ELSE;
+
+			print "\n\n\n========================================================================\n";
+			print $cmd . "\n\n";
+			system( $cmd );
+			open IN, $test_html or die;
+
+			$report_beginning_section = "";
+			while (<IN>) {
+				if( /NAME=\"split0\"/ ) {
+					last;
+				}
+				$report_beginning_section .= $_;
+			}
+		}
 		
 		print OUT "<p><h3>" . ++$count . ". from $test_html</h3></p>\n";
 		print OUT "<p>Command:<br><pre>$cmd<\/pre><\/p>\n";
-		open IN, $test_html or die;
-
-		my $report_beginning_section = "";
-		while (<IN>) {
-			if( /NAME=\"split0\"/ ) {
-				last;
-			}
-			
-			$report_beginning_section .= $_;
-		}
 
 		close IN;
+		print "\t\tparsing $test_html...\n";
+
 
 		if( $report_beginning_section =~ /(<table id=\"master_confusion_matrix\".*?table>)/s ) {
 			$table = $1;
 			print OUT $table;
 			# find the row in the matrix that contains the confusion numbers for this class only
+			my $sum = 0; # used to avoid saving the sum total column in the confusion matrix
 			if( $table =~ /<tr><th>$class<\/th>(.*?)<\/tr>/s ) {
 				$row = $1;
 				my $index = 0;
+				print "confusion matrix \n";
 				while( $row =~ /<td.*?>(\d+)<\/td>/p ) {
+					last if( $1 == $sum ); # we reached the sum total column, just bail here
+					print "$1 ";
+					$sum += $1;
 					$class_confusion_matrix[$index++] += $1;
 					$row = ${^POSTMATCH};
 				}
+				print "\n";
 			} else {
 				die "didn't find class $class row in confusion matrix:\n\n$table\n";
 			}
@@ -147,27 +213,25 @@ foreach $class ( sort &uniq( values %class_list ) ) {
 
 	$num_individuals{$class} = $class_count;
 
-	print OUT "<table border=\"1\" align=\"center\"><caption>Sum of all confusion matrices over population in class $class<\/caption>\n<tr>";
-	for(@class_confusion_matrix) {
-		print OUT "<td>" . $_ . "<\/td>";
-	}
-	print OUT "<\/tr><\/table>\n";
-
-	@{ $confusion_matrix{$class} } = @class_confusion_matrix;
-
-	print OUT "<table border=\"1\" align=\"center\"><caption>Average of average marginal probabilities over population in class $class<\/caption>\n<tr>";
-	for( my $i = 0; $i <= $#class_marg_probs; ++$i ) {
-		$class_marg_probs[$i] = sprintf( "%.3f", $class_marg_probs[$i] / $class_count );
-		print OUT "<td>" . $class_marg_probs[$i] . "<\/td>";
-	}
-	print OUT "<\/tr><\/table>\n";
+#	print OUT "<table border=\"1\" align=\"center\"><caption>Sum of all confusion matrices over population in class $class<\/caption>\n<tr>";
+#	for(@class_confusion_matrix) {
+#		print OUT "<td>" . $_ . "<\/td>";
+#	}
+#	print OUT "<\/tr><\/table>\n";
+#
+#	@{ $confusion_matrix{$class} } = @class_confusion_matrix;
+#
+#	print OUT "<table border=\"1\" align=\"center\"><caption>Average of average marginal probabilities over population in class $class<\/caption>\n<tr>";
+#	for( my $i = 0; $i <= $#class_marg_probs; ++$i ) {
+#		$class_marg_probs[$i] = sprintf( "%.3f", $class_marg_probs[$i] / $class_count );
+#		print OUT "<td>" . $class_marg_probs[$i] . "<\/td>";
+#	}
+#	print OUT "<\/tr><\/table>\n";
 
 	@{ $marg_prob_matrix{$class} } = @class_marg_probs;
 } #end foreach class
 
 close OUT; # all the text printed to out is saved in $out_buffer
-
-
 
 ################################################################
 # NOW PRINT REPORT
@@ -194,30 +258,45 @@ foreach $class ( sort keys %num_individuals ) {
 }
 print OUTPUT_HTML "<th><\/th><th>class total<\/th><th>class accuracy<\/th><\/tr>\n";
 
-my $row = 0;
+$row = 0;
 my $col = 0;
+my $accuracy =0;
+my $std_error_of_mean =0;
+my $confidence_interval =0;
+
 foreach $class ( sort keys %num_individuals ) {
 	$class_total = 0;
 	$class_total_correct = 0;
 	$col = 0;
 	print OUTPUT_HTML "<tr><th>$class ($num_individuals{$class} individuals)<\/th>";
-	foreach ( @{ $confusion_matrix{ $class } } ) {
-		$grand_total += $_;
-		$class_total += $_;
+	for( my $i = 0; $i <= $#{ $confusion_matrix{ $class } }; $i++ ) {
+		my $val = $confusion_matrix{ $class }->[$i];
+		print "class $class row $i val $val\n";
+		$grand_total += $val;
+		$class_total += $val;
 		if( $row == $col ) {
-			print OUTPUT_HTML "<td bgcolor=#D5D5D5>" . $_ . "<\/td>";
-			$grand_total_correct += $_;
-			$class_total_correct += $_;
+			print OUTPUT_HTML "<td bgcolor=#D5D5D5>" . $val . "<\/td>";
+			$grand_total_correct += $val;
+			$class_total_correct += $val;
 		}
 		else {
-			print OUTPUT_HTML "<td>" . $_ . "<\/td>";
+			print OUTPUT_HTML "<td>" . $val . "<\/td>";
 		}
 		++$col;
 	}
-	print OUTPUT_HTML "<td><\/td><td>$class_total<\/td><td>" . sprintf( "%0.3f", $class_total_correct/$class_total ) . "<\/td><\/tr>\n";
+	$accuracy = $class_total_correct/$class_total;
+	$std_error_of_mean = sqrt( $accuracy * (1-$accuracy) / $class_total );
+	$confidence_interval = 1.95996 * $std_error_of_mean;
+
+	print OUTPUT_HTML "<td><\/td><td>$class_total<\/td><td>" . sprintf( "%0.4f", $accuracy ) . " +/- " . sprintf( "%0.4f", $confidence_interval ) . "<\/td><\/tr>\n";
 	++$row;
 }
-print OUTPUT_HTML "<\/table><br><div align=\"center\">Total accuracy: " .  sprintf( "%0.3f", $grand_total_correct/$grand_total ) . "<\/div><\/p>\n";
+
+$accuracy = $grand_total_correct/$grand_total;
+$std_error_of_mean = sqrt( $accuracy * (1-$accuracy) / $grand_total );
+$confidence_interval = 1.95996 * $std_error_of_mean;
+
+print OUTPUT_HTML "<\/table><br><div align=\"center\">Total accuracy: " .  sprintf( "%0.4f", $accuracy) . " +/- " . sprintf( "%0.4f", $confidence_interval ) . "<\/div><\/p>\n";
 
 
 # AGGREGATE CLASS MARGINAL PROBABILITY MATRIX
