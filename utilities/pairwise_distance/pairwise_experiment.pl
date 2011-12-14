5e8748cc03445ac1019bb44618510caec0804f37
christopher.coletta@nih.gov <christopher.coletta@nih.gov@d860a611-dd6a-fbde-3e4e-7fa7191099b5>
2011-12-14 21:46:24 +0000

    pairwise_distance.pl: functionality that will allow the parsing of html files made with wndchrm trunk rev >291, i.e., reports that include error bars within average class probability matrices.

--- utilities/pairwise_distance/pairwise_experiment.pl
+++ utilities/pairwise_distance/pairwise_experiment.pl
@@ -18,6 +18,7 @@ use HTML::Tree;
 
 my @htmlfile_list = split( "\n", `find . -name "*.html"` );
 my %class_info; # Two dimensional hash stores the names and pairwise distances 
+my %class_info_with_deltas; # will include plus minus error
 if( $#htmlfile_list <= 0 ) {
 	die "Dude, no friggin dend files in this dir, you idiot!\n";
 }
@@ -50,7 +51,11 @@ for( my $i = 0; $i <= $#htmlfile_list; $i++) {
 	if( !defined $class_pm_table_element ) {
 		die "Didn't find the average class probability table for file $html_file!!\n";
 	}
+
+	my $using_error_bars = 0;
 	my @dm = ();
+	my @dm_errors = ();
+	my @dm_plus_delta = ();
 	my @vectorON = ();
 	my @vectorOFF = ();
 	my @cv = ();
@@ -62,20 +67,52 @@ for( my $i = 0; $i <= $#htmlfile_list; $i++) {
 		my @cols = ();
 		@cols = $rows[$i]->look_down("_tag","td");
 		foreach (@cols) {
-			push @{ $dm[$i-1] }, $_->as_text;
+			my $val = $_->as_text;
+			if( $val =~ /(\d?\.\d+) +\/- (\d?\.\d+)/)
+			{
+				$using_error_bars = 1;
+				my $mean = $1;
+				my $error_bar = $2;
+				push @{ $dm[$i-1] }, $mean;
+				push @{ $dm_errors[$i-1] }, $error_bar;
+				push @{ $dm_plus_delta[$i-1] }, ($mean + $error_bar);
+			}
+			else
+			{
+				push @{ $dm[$i-1] }, $val;
+			}
 		}
 	}
 	$tree->delete();
 	
 	# Print out the matrix we just absorbed from the html file
-	my $count = 0;
+	print "\nMarginal probability data:\n";
 	foreach my $row ( @dm ) {
 		print "\t";
 		foreach my $avg_marg_prob ( @{ $row } ) {
 			print $avg_marg_prob . " ";
 		}
 		print "\n";
-		$count ++;
+	}
+
+	if( $using_error_bars ) {
+		print "\nMatrix of uncertainties:\n";
+		foreach my $row ( @dm_errors ) {
+			print "\t";
+			foreach my $avg_marg_prob ( @{ $row } ) {
+				print $avg_marg_prob . " ";
+			}
+			print "\n";
+		}
+
+		print "\nMatrix of data plus uncertainties:\n";
+		foreach my $row ( @dm_plus_delta ) {
+			print "\t";
+			foreach my $avg_marg_prob ( @{ $row } ) {
+				print $avg_marg_prob . " ";
+			}
+			print "\n";
+		}
 	}
 	#print " 3.0 (" . $dm[3][0]; print ")- 1.0 (" . $dm[1][0]; print "), 3.1 (" . $dm[3][1]; print ") - 1.1 (" . $dm[1][1]; print "), 3.2 (" . $dm[3][2]; print ")- 1.2 (" . $dm[1][2]; print "), 3.3 (" . $dm[3][3]; print ")- 1.3 (" . $dm[1][3] . ") = ";
 	@vectorON = ( ($dm[3][0]-$dm[1][0]), ($dm[3][1]-$dm[1][1]), ($dm[3][2]-$dm[1][2]), ($dm[3][3]-$dm[1][3]) );
@@ -88,6 +125,20 @@ for( my $i = 0; $i <= $#htmlfile_list; $i++) {
 	print "\tmagnitude of result vector: " . $correctedvector_MAG . "\n";
 	$class_info{$gene1}{$gene2} = $correctedvector_MAG;
 	$class_info{$gene2}{$gene1} = $correctedvector_MAG;
+	
+	if( $using_error_bars ) {
+		# now calculate the morphological divergence score with all the errors added in:
+		@vectorON = ( ($dm_plus_delta[3][0]-$dm_plus_delta[1][0]), ($dm_plus_delta[3][1]-$dm_plus_delta[1][1]), ($dm_plus_delta[3][2]-$dm_plus_delta[1][2]), ($dm_plus_delta[3][3]-$dm_plus_delta[1][3]) );
+		print "\tGENE ON vector (row 4 - row 2):"; foreach (@vectorON) { print $_ . ", "; }; print "\n";
+		@vectorOFF =( ($dm_plus_delta[2][0]-$dm_plus_delta[0][0]), ($dm_plus_delta[2][1]-$dm_plus_delta[0][1]), ($dm_plus_delta[2][2]-$dm_plus_delta[0][2]), ($dm_plus_delta[2][3]-$dm_plus_delta[0][3]) );
+		print "\tGENE OFF vector (row 3 - row 1): "; foreach (@vectorOFF) { print $_ . ", "; }; print "\n";
+		@cv = ( ($vectorON[0]-$vectorOFF[0]), ($vectorON[1]-$vectorOFF[1]), ($vectorON[2]-$vectorOFF[2]), ($vectorON[3]-$vectorOFF[3]) );
+		print "\tresult vector (ON-OFF): "; foreach (@cv) { print $_ . ", "; } print "\n";
+		$correctedvector_MAG = sqrt( $cv[0]**2 + $cv[1]**2 + $cv[2]**2 + $cv[3]**2 );
+		print "\tmagnitude of result vector: " . $correctedvector_MAG . "\n";
+		$class_info_plus_delta{$gene1}{$gene2} = $correctedvector_MAG;
+		$class_info_plus_delta{$gene2}{$gene1} = $correctedvector_MAG;	
+	}
 }
 open OUTPUT, ">master_dendfile_CLASS_PROBABILITIES.txt" or die "Error, process aborted: Can't open output file: $!\n";
 
@@ -108,4 +159,25 @@ foreach my $row ( @master_gene_list ) {
 }
 
 close OUTPUT;
+
+if( $using_error_bars ) {
+	open OUTPUT, ">master_dendfile_CLASS_PROBABILITIES_with_uncertainty.txt" or die "Error, process aborted: Can't open output file: $!\n";
+
+	my $thecount = $#master_gene_list + 1;
+	print OUTPUT "$thecount\n";
+
+	foreach my $row ( @master_gene_list ) {
+		printf( OUTPUT "%s                 ", $row);
+		foreach my $col ( @master_gene_list ) {
+			if( defined $class_info{$row}{$col} ) {
+				printf OUTPUT "%0.4f +/- %0.4f", $class_info{$row}{$col}, abs( $class_info{$row}{$col} - $class_info_plus_delta{$row}{$col} );
+			} else {
+				print OUTPUT "0.0000             ";
+			}
+		}
+		print OUTPUT "\n";
+	}
+
+	close OUTPUT;
+}
 
