99504e025764964caf9841c80a4c20fc4f8f3d6c
christopher.coletta@nih.gov <christopher.coletta@nih.gov@d860a611-dd6a-fbde-3e4e-7fa7191099b5>
2012-01-25 17:22:58 +0000

    PYCHARM: rearranging directories

--- wndchrm/branches/py_chrm/textures/zernike/zernike.h
+++
@@ -1,9 +0,0 @@
-//---------------------------------------------------------------------------
-
-#ifndef zernikeH
-#define zernikeH
-//---------------------------------------------------------------------------
-
-void mb_zernike2D(ImageMatrix *I, double D, double R, double *zvalues, long *output_size);
-
-#endif
